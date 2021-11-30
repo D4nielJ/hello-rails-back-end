8ef3831afe0187f615d8d64e866ddade96f6c147
d4nielj <d4niel.djm@gmail.com>
2021-11-30 18:31:27 -0500

    Add random phrases to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..20).each do |_e|
+  Greeting.create!(phrase: Faker::Books::Lovecraft.sentence)
+end
